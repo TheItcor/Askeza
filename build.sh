7e66fc1e2a2b746140ea0f24c321ee7dd738c689
TheItcor <itcorsmail@gmail.com>
2026-01-03 02:24:56 +0300

    add build bash script, src/, include/

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# I think complex build systems are unnecessary. Because there are few files.
+# The bash-script solves all problems:
+
+
+# Compiler: Clang
+
+# Find all files.c in src/
+C_FILES=$(find src -name "*.c")
+
+# Find all headers.h in includes/
+H_FILES=$(find include -name "*.h")
+
+# Compile
+clang $C_FILES -Iinclude -o ask
